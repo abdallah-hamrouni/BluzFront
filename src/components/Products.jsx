9faaf3604e84a1af9d6e7ecffac2fdae0342fb35
abdallah-hamrouni <abdallahhamrouni24@gmail.com>
2025-04-16 19:01:04 +0100

    gg

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,293 +1,124 @@
 import Header from './Header';
 import Footer from './Footer';
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom"; // Assure-toi d'importer Link
+import { toast } from "react-toastify";
+const Products = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const Products =() =>{
-    return(
-        
-<>
-    <Header/>
-<div> 
-  <div className="page-heading" id="top">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="inner-content">
-            <h2>Check Our Products</h2>
-            <span>Awesome &amp; Creative HTML CSS layout by TemplateMo</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  {/* ***** Main Banner Area End ***** */}
-  {/* ***** Products Area Starts ***** */}
-  <section className="section" id="products">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="section-heading">
-            <h2>Our Latest Products</h2>
-            <span>Check out all of our products.</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/men-01.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Classic Spring</h4>
-              <span>$120.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/men-02.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Air Force 1 X</h4>
-              <span>$90.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/men-03.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Love Nana ‘20</h4>
-              <span>$150.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/women-01.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>New Green Jacket</h4>
-              <span>$75.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/women-02.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Classic Dress</h4>
-              <span>$45.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/women-03.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Spring Collection</h4>
-              <span>$130.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/kid-01.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>School Collection</h4>
-              <span>$80.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/kid-02.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Summer Cap</h4>
-              <span>$12.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="item">
-            <div className="thumb">
-              <div className="hover-content">
-                <ul>
-                  <li><a href="single-product.html"><i className="fa fa-eye" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-star" /></a></li>
-                  <li><a href="single-product.html"><i className="fa fa-shopping-cart" /></a></li>
-                </ul>
-              </div>
-              <img src="assets/images/kid-03.jpg" alt="" />
-            </div>
-            <div className="down-content">
-              <h4>Classic Kid</h4>
-              <span>$30.00</span>
-              <ul className="stars">
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-                <li><i className="fa fa-star" /></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-12">
-          <div className="pagination">
-            <ul>
-              <li>
-                <a href="/">1</a>
-              </li>
-              <li className="active">
-                <a href="/">2</a>
-              </li>
-              <li>
-                <a href="/">3</a>
-              </li>
-              <li>
-                <a href="/">4</a>
-              </li>
-              <li>
-                <a href="/">&gt;</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-</div>
-    <Footer/>
-</>
-    )
-}
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://bluz-backend.onrender.com/api/products");
+        setItems(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-export default Products
+    fetchProducts();
+  }, []);
+
+  const addToCart = async (productId, quantity = 1) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('No token found, please login');
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "https://bluz-backend.onrender.com/api/carts/addToCart",
+        { productId, quantity },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    toast.success('Product added to cart successfully!');
+    } catch (error) {
+      console.error('Error adding to cart:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="page-heading" id="top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="inner-content">
+                <h2>Check Our Products</h2>
+                <span>Awesome & Creative collection</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <section className="section" id="products">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-heading">
+                <h2>Our Latest Products</h2>
+                <span>Check out all of our products.</span>
+              </div>
+            </div>
+          </div>
+  
+          <div className="row">
+            {items.length === 0 ? (
+              <div>Loading...</div>
+            ) : (
+              items.map((item) => (
+                <div className="col-lg-4 col-md-6 mb-4" key={item._id}>
+                  <div className="item">
+                    <div className="thumb">
+                      <div className="hover-content">
+                        <ul>
+                          <li>
+                            <Link to={`/singleProduct/${item._id}`}>
+                              <i className="fa fa-eye" />
+                            </Link>
+                          </li>
+                          <li>
+                            <Link onClick={() => addToCart(item._id, 1)}>
+                              <i className="fa fa-shopping-cart" />
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      <Link to={`/singleProduct/${item._id}`}>
+                        <img
+                          alt={item.name}
+                          style={{
+                            height: "480px",
+                            width: "100%",
+                            objectFit: "cover",
+                            cursor: "pointer",
+                          }}
+                          className="mx-1"
+                          src={item.colors?.[0]?.image || 'default.jpg'}
+                        />
+                      </Link>
+                    </div>
+                    <div className="down-content">
+                      <h4>{item.name}</h4>
+                      <span>${item.price}</span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+  
+      <Footer />
+    </>
+  );
+};
+
+export default Products;
